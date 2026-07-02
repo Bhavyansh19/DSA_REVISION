9745b3a2e8d1ed3877109b624e8c53ad39df5ecc
Bhavyansh Jain <88175199+Bhavyansh19@users.noreply.github.com>
2026-07-02 15:22:09 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int n = nums.length;
+        Set<Integer> repeat = new HashSet<>();
+
+        for(int i = 0; i < n; i++){
+            if(repeat.contains(nums[i])){
+                return true;
+            }
+            repeat.add(nums[i]);
+        }
+        return false;
+    }
+}
